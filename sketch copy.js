843f762d625029f4076e308306269b03d66cea24
PingPang2 <86273018+PingPang2@users.noreply.github.com>
2021-07-13 18:08:12 -0700

    Update sketch copy.js

--- sketch copy.js
+++ sketch copy.js
@@ -8,9 +8,11 @@ function setup() {
 speed=random(223,321);
 weight=random(30,52);
 
-bullet=createSprite(50,200,80,30);
+bullet=createSprite(50,200,80,10);
 bullet.velocityX=speed;
-bullet.shapeColor=color("white");
+bullet.shapeColor=color(100);
+  
+thickness=random(22,83);
 
 wall=createSprite(1200,200,thickness,height/2);
 wall.shapeColor=color(80,80,80);
@@ -26,20 +28,18 @@ if(wall.x-bullet.x<(bullet.width+wall.width)/2){
 bullet.velocityX=0;
 var deformation=0.5*weight*speed*speed/22500;
 
-if(deformation>180){
-  bullet.shapeColor=color(255,0,0);
+if(deformation>10){
+  wall.shapeColor=color(255,0,0);
 }
-if(deformation<180&&deformation>100){
-  bullet.shapeColor=color(230,230,0);
+if(deformation<10){
+  wall.shapeColor=color(230,230,0);
 }
-if(deformation<100){
-  bullet.shapeColor=color(0,255,0);
- }
+
 }
 
 if(hasCollided(bullet,wall)){
   bullet.velocityX=0;
-  var damage=0.5*wieght*speed*speed/(thickness*thickness*thickness);
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 
   
   if(damage>10){
@@ -50,8 +50,8 @@ if(hasCollided(bullet,wall)){
   drawSprites();
 }
 function hasCollided(Lbullet,Lwall){
- bulletRightEdge=lbullet.x +lbullet.width;
- wallLeftEdge=lwall.x;
+ bulletRightEdge=bullet.x +bullet.width;
+ wallLeftEdge=wall.x;
  if(bulletRightEdge>=wallLeftEdge){
    return true
  }
